a1eb686ba6ae1d06ae97c16d80c929e326c2c609
abhinit18 <rabhinit@gmail.com>
2017-04-11 15:46:55 +0530

    Update scoreStatistics-Ctrl.js
    changes done

--- app/components/Donation/scoreStatistics-Ctrl.js
+++ app/components/Donation/scoreStatistics-Ctrl.js
@@ -3,16 +3,19 @@
  */
 App.controller('StatsController', function ($scope, $http, $cookies, $cookieStore, $state) {
 //console.log(MY_CONSTANT.url);
-    $scope.SubmitValue = function(){
+    $scope.changeValue = function(){
         console.log($scope.DonationValue);
         if($scope.DonationValue == null || $scope.DonationValue == undefined){
             alert("Please enter the donation ammount.")
         }else{
-             var maxValue,allotedValue;
+            var maxValue,allotedValue;
             maxValue = 200;
             allotedValue = ($scope.DonationValue/maxValue)*100;
             $('#progress-bar').css('width',allotedValue+"%").attr('aria-valuenow',$scope.DonationValue);
         }
+    }
+    $scope.SubmitValue = function(){
+        alert("congrats you have donated"+"$"+$scope.DonationValue);
     };
 //    ======================================= New Chart ==================================== //
 
